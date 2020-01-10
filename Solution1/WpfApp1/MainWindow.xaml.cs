983a71660898675f0db9c1a1367d6f93f2083d62
tmp20190820 <54312936+tmp20190820@users.noreply.github.com>
2020-01-10 21:48:21 +0900

    .

--- Solution1/WpfApp1/MainWindow.xaml.cs
+++ Solution1/WpfApp1/MainWindow.xaml.cs
@@ -10,61 +10,58 @@ namespace WpfApp1
 	public partial class MainWindow : Window
 	{
 
-		private readonly Dictionary<string, ClassListView> listViews = new Dictionary<string, ClassListView>();
+		private readonly Dictionary<string, dynamic> listViews = new Dictionary<string, dynamic>();
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			listViews.Add("1", new ClassListView(listView1));
-			listViews.Add("2", new ClassListView(listView2));
-			listViews.Add("3", new ClassListView(listView3));
-			listViews.Add("4", new ClassListView(listView4));
+			listViews.Add("1", new ListViewType1(listView1));
+			listViews.Add("2", new ListViewType1(listView2));
+			listViews.Add("3", new ListViewType2(listView3));
 
 			listView1.ItemsSource = listViews["1"].items; // 바인딩
 			listView2.ItemsSource = listViews["2"].items;
 			listView3.ItemsSource = listViews["3"].items;
-			listView4.ItemsSource = listViews["4"].items;
 		}
-		
+
 		private void ListView_DragEnter(object sender, DragEventArgs e) // 파일 드롭시 마우스 커서 변경
 		{
 			e.Effects = DragDropEffects.Copy;
 		}
-		
+
 		private void ListView_Drop(object sender, DragEventArgs e) // 드롭 이벤트
 		{
 			switch (GetTabItem())
 			{
 				case "1":
-					GetListView(sender).Drop(e);
+					DropFiles(e, GetListView(sender));
 					break;
 				case "2":
-					SetTempName(e);
+					DropFiles(e, listViews["3"]);
 					break;
 			}
 		}
-		
+
 		private void ListViewMenuItemRemove_Click(object sender, RoutedEventArgs e) // 삭제 클릭
 		{
-			GetListView(sender).ItemRemove();
+			ItemRemove(GetListView(sender));
 		}
 
 		private void ListViewMenuItemClear_Click(object sender, RoutedEventArgs e) // 클리어 클릭
 		{
-			GetListView(sender).ItemClear();
+			ItemClear(GetListView(sender));
 		}
-		private void MainMenuItemClear_Click(object sender, RoutedEventArgs e)
+		private void MainMenuItemClear_Click(object sender, RoutedEventArgs e) // 클리어 클릭
 		{
 			switch (GetTabItem())
 			{
 				case "1":
-					listViews["1"].ItemClear();
-					listViews["2"].ItemClear();
+					ItemClear(listViews["1"]);
+					ItemClear(listViews["2"]);
 					break;
 				case "2":
-					listViews["3"].ItemClear();
-					listViews["4"].ItemClear();
+					ItemClear(listViews["3"]);
 					break;
 			}
 		}
@@ -74,10 +71,10 @@ namespace WpfApp1
 			switch (GetTabItem())
 			{
 				case "1":
-					Rename(listViews["1"], listViews["2"], "copyname");
+					Rename(listViews["1"], listViews["2"], "run");
 					break;
 				case "2":
-					Rename(listViews["3"], listViews["4"], "tempname");
+					Rename(listViews["3"], "run");
 					break;
 			}
 		}
@@ -90,24 +87,39 @@ namespace WpfApp1
 					Rename(listViews["1"], listViews["2"], "restore");
 					break;
 				case "2":
-					Rename(listViews["3"], listViews["4"], "restore");
+					Rename(listViews["3"], "restore");
 					break;
 			}
 		}
 
+		private void ListViewMenuItemTempName_Click(object sender, RoutedEventArgs e) // TempName
+		{
+			SetTempName(listViews["3"]);
+		}
+
 		private void ListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) // 드래그 시작
 		{
-			GetListView(sender).DragStart();
+			DragStart(GetListView(sender));
 		}
 
 		private void ListView_PreviewMouseMove(object sender, MouseEventArgs e) // 드래그 동작
 		{
-			GetListView(sender).DragMouseMove();
+			DragMouseMove(GetListView(sender));
 		}
 
 		private void ListView_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) // 드래그 중지
 		{
-			GetListView(sender).DragStop();
+			DragStop();
+		}
+
+		private void TextBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e) // 마우스휠
+		{
+			MouseWheelNumber(sender, e);
+		}
+
+		private void ComboBox1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) // 선택 변경
+		{
+			ComboBox1SelectionChanged();
 		}
 	}
 }
